156878e70b490d42f30e1e84005c50bb4be9d375
Benz <benzsanbenz@gmail.com>
2020-09-27 20:28:20 +0700

    Your message about the commit

---
+++ build/precache-manifest.7a4794a179a351d7480e57e865902ec4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80fa3cbb19bbb06b0a5bab68f92cc2f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "deb949ed540a2a2c1939",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6333194b5a93a9709a4b",
+    "url": "/static/js/2.c6affc82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c6affc82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deb949ed540a2a2c1939",
+    "url": "/static/js/main.1ccde42f.chunk.js"
+  },
+  {
+    "revision": "8c7c8ae5796d6d30bdd1",
+    "url": "/static/js/runtime-main.eb9aa1c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
